ade8797e4986f29e362f0252e67af7e1ab8f89e6
bjorn.vestre <bjorn.vestre.vestre@hva.nl>
2025-10-29 21:56:16 +0100

    qr code generator

---
+++ qr_code.py
@@ -0,0 +1,10 @@
+from qrcode.main import QRCode
+
+
+data = input("Enter the text or URL").strip()
+filename = input("Enter the filename: ").strip()
+qr = QRCode(box_size=10, border=4)
+qr.add_data(data)
+image = qr.make_image(fill_color="black", back_color="white")
+image.save(filename)
+print(f'QR code saved as "{filename}"')
